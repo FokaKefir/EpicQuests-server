bfdc4a9d9afa5bf21e243f19563f98058085a46c
FokaKefir <babosdavid8@gmail.com>
2024-03-23 20:25:30 +0200

    add image upoload function through http

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 from utils import *
 import os
+import uuid
 
 # input and generated path
 input_path = os.getcwd() + '/data/input/'
@@ -12,6 +16,21 @@ model = get_model(2, model_path + 'im3vx6dd_generator.keras')
 
 # create app
 app = FastAPI()
+
+# add cors
+
+origins = [
+    "http://localhost",
+    "http://localhost:2000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # image generator call
 @app.get("/generate/{image_name}")
@@ -34,3 +53,16 @@ async def generate_image(image_name: str):
 
     # return url to generated image
     return {"image_path": f"fokakefir.go.ro/generated/{image_name}"}
+
+# file uploader
+@app.post('/upload/')
+async def upload_file(file: UploadFile = File(...)):
+
+    file.filename = f'{uuid.uuid4()}.jpg'
+    contents = await file.read()
+
+    # save the file
+    with open(f'{input_path}{file.filename}', 'wb') as fout:
+        fout.write(contents)
+
+    return {'filename': file.filename}  
